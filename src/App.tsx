2bd9706119a8741f5ba9c76822d3ec5f0a032a85
Junior <junior.solo@hotmail.com>
2026-02-17 01:49:19 -0300

    Add CSS component with react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading';
+
+
 function App(){
 
     console.log('strict mode');
     return (
     <>
-        <h2>Ola mundo app</h2>
+        <Heading/>
         <h3>Teste app </h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repellat odit nulla aperiam unde exercitationem nostrum consequuntur delectus atque veniam neque iure doloremque mollitia laborum, praesentium sunt pariatur perferendis harum.</p>
     </>  
---
+++ src/components/Heading.tsx
@@ -0,0 +1,5 @@
+import styles from './Heading.module.css';
+
+export function Heading(){
+    return   <h2 className={`${styles.black} ${styles.heading}`}>Ola mundo app</h2>
+}
