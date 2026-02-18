0b2cba5f86f612db94b1260a304fe741c8c8ae56
Junior <junior.solo@hotmail.com>
2026-02-17 22:52:26 -0300

    set paramters for heading component.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(){
     console.log('strict mode');
     return (
     <>
-        <Heading/>
+        <Heading attr={123} attr2='teste'>Ola mundo!</Heading>
         <h3>Teste app </h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repellat odit nulla aperiam unde exercitationem nostrum consequuntur delectus atque veniam neque iure doloremque mollitia laborum, praesentium sunt pariatur perferendis harum.</p>
     </>  
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,6 @@
 import styles from './Heading.module.css';
 
-export function Heading(){
-    return   <h2 className={`${styles.black} ${styles.heading}`}>Ola mundo app</h2>
+export function Heading(props){
+    console.log(props);
+    return   <h2 className={`${styles.black} ${styles.heading}`}>{props.children}</h2>
 }
