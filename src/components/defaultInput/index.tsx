2ef949610e16b904882d459d6ab13740f7843f3a
Junior <junior.solo@hotmail.com>
2026-02-20 00:16:01 -0300

    Implementing default input.

--- src/components/defaultInput/index.tsx
+++ src/components/defaultInput/index.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css';
+
 type DefaultInputProp = {
   id: string;
   labelText?: string;
@@ -7,7 +9,7 @@ export function DefaultInput( { id , type, labelText, ...rest} : DefaultInputPro
   return(
     <>
       { labelText && <label htmlFor={id}>{labelText}</label> }
-      <input id={id} type={type} {...rest}/>
+      <input className={styles.input} id={id} type={type} {...rest}/>
     </>
   );
 }
