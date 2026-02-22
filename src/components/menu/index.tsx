83ca9cbe831b230c92c74089793eb7b733d8d199
Junior <junior.solo@hotmail.com>
2026-02-22 15:55:55 -0300

    userEffect implementation, use case.

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 
@@ -9,9 +9,29 @@ export function Menu(){
     const [theme, setTheme] = useState<AvailableThemes>('dark');
 
     function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
-        event.preventDefault();
-        console.log('clicado', Date.now());
+        event.preventDefault(); // nao segue o link
+        setTheme(prevTheme => {
+            const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+            return nextTheme; 
+        });
     }
+
+     // useEffect(() => {
+  //   console.log('useEffect sem dependências', Date.now());
+  // }); // Executado todas vez que o componente renderiza na tela
+
+  // useEffect(() => {
+  //   console.log('useEffect com array deps vazio', Date.now());
+  // }, []); // Executa apenas quando o React monta o componente na tela pela primeira vez
+
+    useEffect(() => {
+    console.log('Theme mudou', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('Olha, este componente será atualizado');
+    };
+  }, [theme]); 
 
     return  (
     <nav className={`${styles.menu}`}>
